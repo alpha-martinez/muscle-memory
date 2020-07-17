28cd43ac6a9a20a0c005d94e1e09fd869b3c5eb5
Alpha Martinez <alphamartinez@Alphas-MacBook-Pro.local>
2020-07-17 17:08:06 -0400

    [variables] add 5 variables

---
+++ app.js
@@ -0,0 +1,11 @@
+let myName = "My name is Alpha";
+let num = 24;
+const colors = ["blue", "red", "white", "black"]; //use const for arrays
+let isLoggedIn = true;
+const places = { //use const when using objects
+    favPlace: "New York",
+    yearsHere: 14,
+    goodFood: ["Pizza", "Chinese food", "mexican"],
+    loveItHere: true
+}
+//to access your key values it should be places.faveplace
